2515b94798330887f92888300e5c3ad9ac83d5dd
mcwolfe <mcwolfemedia@gmail.com>
2024-09-23 02:08:42 +0200

    mer nvm

--- installTool_linux.sh
+++ installTool_linux.sh
@@ -68,6 +68,9 @@ done
 install_flatpak
 curl -fsSL https://install.julialang.org | sh
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 # Example of installing an app with Flatpak if needed
 # install_with_flatpak "com.visualstudio.code"
 
